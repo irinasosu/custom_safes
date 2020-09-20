2b136e23fae33ad8da704f670fbb7f26324f9e50
Irina Sosu <irinasosu79@gmail.com>
2020-09-19 22:59:58 -0400

    Initial Commit

---
+++ safe-choise.js
@@ -0,0 +1,194 @@
+	let safe_code_list = {
+    "M5B_1BRM": "6M5_1FB-KW",
+    "M5B_1BRC": "6M5_1FB-KM",
+    "M5B_1BRN": "6M5_1FB",
+    "M5B_1BLM": "6M5_1FBL-KW",
+    "M5B_1BLC": "6M5_1FBL-KM",
+	"M5B_1BLN": "6M5_1FBL",
+	"M5_1RM1B": "6M5_1BB-KW",
+	"M5_1RC1B": "6M5_1BB-KM",
+	"M5_1RN1B": "6M5_1BB",
+	"M5_1LM1B": "6M5_1BBL-KW",
+	"M5_1LC1B": "6M5_1BBL-KM",
+	"M5_1LN1B": "6M5_1BBL",
+	"M5_1RM1W": "6M5_19003-KW",
+	"M5_1RC1W": "6M5_19003-KM",
+	"M5_1RN1W": "6M5_19003",
+	"M5_1LM1W": "6M5_19003L-KW",
+	"M5_1LC1W": "6M5_19003L-KM",
+	"M5_1LN1W": "6M5_19003L",
+	"AN5_3RMB": "6AN5_3BB-KW",
+	"AN5_3RCB": "6AN5_3BB-KM",
+	"AN5_3RNB": "6AN5_3BB",
+	"AN5_3LMB": "6AN5_3BBL-KW",
+	"AN5_3LCB": "6AN5_3BBL-KM",
+	"AN5_3LNB": "6AN5_3BBL",
+	"AN5_3RMW": "6AN5_3WW-KW",
+	"AN5_3RCW": "6AN5_3WW-KM",
+	"AN5_3RNW": "6AN5_39003",
+	"AN5_3LMW": "6AN5_3WWL-KW",
+	"AN5_3LCW": "6AN5_3WWL-KM",
+	"AN5_3LNW": "6AN5_39003L",
+	"T4_0RMB": "6T4_0BB-KW",
+	"T4_0RCB": "6T4_0BB-KM",
+	"T4_0RNB": "6T4_0BB",
+	"T4_0LMB": "6T4_0BBL-KW",
+	"T4_0LCB": "6T4_0BBL-KM",
+	"T4_0LNB": "6T4_0BBL",
+	"DN5_4FMB": "6DN5_4BB-KW",
+	"DN5_4FCB": "6DN5_4BB-KM",
+	"DN5_4FNB": "6DN5_4BB",
+	"DN5_4FMW": "6DN5_49003-KW",
+	"DN5_4FCW": "6DN5_49003-KM",
+	"DN5_4FNW": "6DN5_49003",
+	"W5_0RMB": "6W5_0BB-KW",
+	"W5_0RCB": "6W5_0BB-KM",
+	"W5_0RNB": "6W5_0BB",
+	"W5_0LMB": "6W5_0BBL-KW",
+	"W5_0LCB": "6W5_0BBL-KM",
+	"W5_0LNB": "6W5_0BBL",
+	"W5_0RMW": "6W5_09003-KW",
+	"W5_0RCW": "6W5_09003-KM",
+	"W5_0RNW": "6W5_09003",
+	"W5_0LMW": "6W5_09003L-KW",
+	"W5_0LCW": "6W5_09003L-KM",
+	"W5_0LNW": "6W5_09003L",
+	"E5_1RMB": "6E5_1BB-KW",
+	"E5_1RCB": "6E5_1BB-KM",
+	"E5_1RNB": "6E5_1BB",
+	"E5_1LMB": "6E5_1BBL-KW",
+	"E5_1LCB": "6E5_1BBL-KM",
+	"E5_1LNB": "6E5_1BBL",
+	"E5_1RMW": "6E5_19003-KW",
+	"E5_1RCW": "6E5_19003-KM",
+	"E5_1RNW": "6E5_1WW",
+	"E5_1LMW": "6E5_19003L-KW",
+	"E5_1LCW": "6E5_19003L-KM",
+	"E5_1LNW": "6E5_19003L",
+	"EN5_3RMB": "6EN5_3BB-KW",
+	"EN5_3RCB": "6EN5_3BB-KM",
+	"EN5_3RNB": "6EN5_3BB",
+	"EN5_3LMB": "6EN5_3BBL-KW",
+	"EN5_3LCB": "6EN5_3BBL-KM",
+	"EN5_3LNB": "6EN5_3BBL",
+	"EN5_3RMW": "6EN5_39003-KW",
+	"EN5_3RCW": "6EN5_3WW-KM",
+	"EN5_3RNW": "6EN5_39003",
+	"EN5_3LMW": "6EN5_39003L-KW",
+	"EN5_3LCW": "6EN5_3WWL-KM",
+	"EN5_3LNW": "6EN5_39003L",
+	"DP5_3FMB": "6DP5_3BB-KW",
+	"DP5_3FCB": "6DP5_3BB-KM",
+	"DP5_3FNB": "6DP5_3BB",
+	"DP5_3FMW": "6DP5_39003-KW",
+	"DP5_3FCW": "6DP5_39003-KM",
+	"DP5_3FNW": "6DP5_39003",
+	"WX5_0RMB": "6WX5_0BB-KW",
+	"WX5_0RCB": "6WX5_0BB-KM",
+	"WX5_0RNB": "6WX5_0BB",
+	"WX5_0LMB": "6WX5_0BBL-KW",
+	"WX5_0LCB": "6WX5_0BBL-KM",
+	"WX5_0LNB": "6WX5_0BBL",
+	"WX5_0RMW": "6WX5_09003-KW",
+	"WX5_0RCW": "6WX5_09003-KM",
+	"WX5_0RNW": "6WX5_09OO3",
+	"WX5_0LMW": "6WX5_09003L-KW",
+	"WX5_0LCW": "6WX5_09003L-KM",
+	"WX5_0LNW": "6WX5_09003L",
+	"SX5_0RMB": "6SX5_0BB-KW",
+	"SX5_0RCB": "6SX5_0BB-KM",
+	"SX5_0RNB": "6SX5_0BB",
+	"SX5_0LMB": "6SX5_0BBL-KW",
+	"SX5_0LCB": "6SX5_0BBL-KM",
+	"SX5_0LNB": "6SX5_0BBL",
+	"SX5_0RMW": "6SX5_09003-KW",
+	"SX5_0RCW": "6SX5_09003-KM",
+	"SX5_0RNW": "6SX5_09003",
+	"SX5_0LMW": "6SX5_09003L-KW",
+	"SX5_0LCW": "6SX5_09003L-KM",
+	"SX5_0LNW": "6SX5_09003L",
+	"B5_0FMB": "6B5_0BB-KW",
+	"DDN5_4FMB": "6DN5_4BB-KW",
+	"DDN5_4FCB": "6DN5_4BB-KM",
+	"DDN5_4FNB": "6DN5_4BB",
+	"DDN5_4FMW": "6DN5_49003-KW",
+	"DDN5_4FCW": "6DN5_49003-KM",
+	"DDN5_4FNW": "6DN5_49003",
+	"DDP5_3FMB": "6DP5_3BB-KW",
+	"DDP5_3FCB": "6DP5_3BB-KM",
+	"DDP5_3FNB": "6DP5_3BB",
+	"DDP5_3FMW": "6DP5_39003-KW",
+	"DDP5_3FCW": "6DP5_39003-KM",
+	"DDP5_3FNW": "6DP5_39003",
+	"WW5_0RMB": "6W5_0BB-KW",
+	"WW5_0RCB": "6W5_0BB-KM",
+	"WW5_0RNB": "6W5_0BB",
+	"WW5_0LMB": "6W5_0BBL-KW",
+	"WW5_0LCB": "6W5_0BBL-KM",
+	"WW5_0LNB": "6W5_0BBL",
+	"WW5_0RMW": "6W5_09003-KW",
+	"WW5_0RCW": "6W5_09003-KM",
+	"WW5_0RNW": "6W5_09003",
+	"WW5_0LMW": "6W5_09003L-KW",
+	"WW5_0LCW": "6W5_09003L-KM",
+	"WW5_0LNW": "6W5_09003L",
+	"WWX5_0RMB": "6WX5_0BB-KW",
+	"WWX5_0RCB": "6WX5_0BB-KM",
+	"WWX5_0RNB": "6WX5_0BB",
+	"WWX5_0LMB": "6WX5_0BBL-KW",
+	"WWX5_0LCB": "6WX5_0BBL-KM",
+	"WWX5_0LNB": "6WX5_0BBL",
+	"WWX5_0RMW": "6WX5_09003-KW",
+	"WWX5_0RCW": "6WX5_09003-KM",
+	"WWX5_0RNW": "6WX5_09003",
+	"WWX5_0LMW": "6WX5_09003L-KW",
+	"WWX5_0LCW": "6WX5_09003L-KM",
+	"WWX5_0LNW": "6WX5_09003L",
+	"WSX5_0RMB": "6SX5_0BB-KW",
+	"WSX5_0RCB": "6SX5_0BB-KM",
+	"WSX5_0RNB": "6SX5_0BB",
+	"WSX5_0LMB": "6SX5_0BBL-KW",
+	"WSX5_0LCB": "6SX5_0BBL-KM",
+	"WSX5_0LNB": "6SX5_0BBL",
+	"WSX5_0RMW": "6SX5_09003-KW",
+	"WSX5_0RCW": "6SX5_09003-KM",
+	"WSX5_0RNW": "6SX5_09003",
+	"WSX5_0LMW": "6SX5_09003L-KW",
+	"WSX5_0LCW": "6SX5_09003L-KM",
+	"WSX5_0LNW": "6SX5_09003L"
+};
+
+function displaySafeSelected() { 
+
+	console.log('on click submit button');
+
+	let safe_selected_key = "";
+	
+	//document.getElementById("result").innerHTML = "";
+
+	let elementSelected = document.getElementsByTagName('input');
+
+	let safe_item = "";
+	
+	for(i = 0; i < elementSelected.length; i++) { 
+		
+		if(elementSelected[i].type="radio"); { 
+		
+			if (elementSelected[i].checked) 
+			
+			safe_selected_key+=elementSelected[i].value;
+
+			elementSelected[i].checked = false;
+		}
+	}
+
+	console.log('safe sected is:' + safe_selected_key);
+
+	for (safe_item in safe_code_list){
+
+		if (safe_item == safe_selected_key) {
+		
+		alert(safe_code_list[safe_item] + "\n" + "Price: 1 - 50 = $125" + "\n" + "Price: 51 - 200 = $115");
+	}}
+		console.log(safe_code_list[safe_item]);
+}
